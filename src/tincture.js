80caa2cc7b883c88214e6d71e830c832d40810ec
Raine Luntta <raine.luntta@gmail.com>
2019-05-27 20:59:34 +0300

    [MOD] refactoring and new features

--- src/tincture.js
+++ src/tincture.js
@@ -6,192 +6,349 @@ function tincture(color, options) {
 
 	if (color instanceof tincture) {
 		return color;
-	};
+	}
 
 	if (!(this instanceof tincture)) {
 		return new tincture(color, options);
 	}
 
-	let output = {
-		original: color
-	};
+	this._original = color;
 
-	output.originalFormat = this.getFormat(color);
-	switch (output.originalFormat) {
+	this._originalFormat = this.getFormat(color);
+	switch (this._originalFormat) {
 		case "RGB":
-			output.hasAlpha = false;
-			output.rgb = color;
-			output.hsl = this.RGBToHSL(color, true);
-			output.hex = this.RGBToHEX(color);
-
+			this.hasAlpha = false;
+			this.rgb = this.RGBStringToRGBObj(color, this.hasAlpha);
 			break;
 		case "RGBA":
-			output.hasAlpha = true;
-			output.rgb = color;
-			output.hsl = this.RGBAToHSLA(color);
-			output.hex = this.RGBAToHEXA(color);
-
+			this.hasAlpha = true;
+			this.rgb = this.RGBStringToRGBObj(color, this.hasAlpha);
 			break;
 		case "HEX":
-			output.hasAlpha = false;
-			output.hex = color.toLowerCase();
-			output.rgb = this.HEXToRGB(color);
-			output.hsl = this.HEXToHSL(color);
+			this.hasAlpha = false;
+			this.rgb = this.HEXToRGB(color, true);
 			break;
 		case "HEXA":
-			output.hasAlpha = true;
-			output.hex = color.toLowerCase();
-			output.rgb = this.HEXAToRGBA(color);
-			output.hsl = this.HEXAToHSLA(color);
+			this.hasAlpha = true;
+			this.rgb = this.HEXAToRGBA(color, true);
 			break;
 		case "HSL":
-			output.hasAlpha = false;
-			output.hsl = color;
-			output.rgb = this.HSLToRGB(color, true);
-			output.hex = this.HSLToHEX(color, true);
+			this.hasAlpha = false;
+			this.rgb = this.HSLToRGB(color, true);
 			break;
 		case "HSLA":
-			output.hasAlpha = true;
-			output.hsl = color;
-			output.rgb = this.HSLAToRGBA(color, true);
-			output.hex = this.HSLAToHEXA(color, true);
+			this.hasAlpha = true;
+			this.rgb = this.HSLAToRGBA(color, true);
 			break;
 		default:
 			console.error("Invalid input color: " + color);
-			break;
+			return;
 	}
 
-	return output;
+	if (this.hasAlpha) {
+		this.hex = this.RGBAToHEXA(this.RGBObjToRGBString(this.rgb), true);
+		this.hsl = this.RGBAToHSLA(this.RGBObjToRGBString(this.rgb), true);
+	} else {
+		this.hex = this.RGBToHEX(this.RGBObjToRGBString(this.rgb), true);
+		this.hsl = this.RGBToHSL(this.RGBObjToRGBString(this.rgb), true);
+	}
 }
 
 tincture.prototype = {
-	isRGB: function(str) {
+	RGBStringToRGBObj: function(color) {
+		if (this.isRGBAString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
+				.substr(5)
+				.split(")")[0]
+				.split(sep);
+
+			if (color.indexOf("/") > -1) color.splice(3, 1);
+
+			for (let i in color) {
+				let r = color[i];
+				if (r.indexOf("%") > -1) {
+					let p = r.substr(0, r.length - 1) / 100;
+
+					if (i < 3) {
+						color[i] = Math.round(p * 255);
+					} else {
+						color[i] = p;
+					}
+				}
+			}
+
+			let r = color[0],
+				g = color[1],
+				b = color[2],
+				a = color[3];
+
+			return { r: +r, g: +g, b: +b, a: +a };
+		} else if (this.isRGBString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
+				.substr(4)
+				.split(")")[0]
+				.split(sep);
+
+			for (let i in color) {
+				let r = color[i];
+				if (r.indexOf("%") > -1)
+					color[i] = Math.round(
+						(r.substr(0, r.length - 1) / 100) * 255
+					);
+			}
+
+			let r = color[0],
+				g = color[1],
+				b = color[2];
+
+			return { r: +r, g: +g, b: +b };
+		}
+	},
+	RGBObjToRGBString: function(color) {
+		if (typeof color === "object") {
+			if (color.hasOwnProperty("a")) {
+				return (
+					"rgba(" +
+					+color.r +
+					"," +
+					+color.g +
+					"," +
+					+color.b +
+					"," +
+					+color.a +
+					")"
+				);
+			} else {
+				return (
+					"rgb(" + +color.r + "," + +color.g + "," + +color.b + ")"
+				);
+			}
+		}
+	},
+	HSLObjToHSLString: function(color) {
+		if (typeof color === "object") {
+			if (color.hasOwnProperty("a")) {
+				return (
+					"hsla(" +
+					+color.h +
+					"," +
+					+color.s +
+					"%," +
+					+color.l +
+					"%," +
+					+color.a +
+					")"
+				);
+			} else {
+				return (
+					"hsl(" + +color.h + "," + +color.s + "%," + +color.l + "%)"
+				);
+			}
+		}
+	},
+	toRgb: function() {
+		return this.rgb;
+	},
+	toRgbString: function() {
+		return this.RGBObjToRGBString(this.rgb);
+	},
+	toHsl: function() {
+		return this.hsl;
+	},
+	toHslString: function() {
+		return this.HSLObjToHSLString(this.hsl);
+	},
+	toHex: function() {
+		return this.hex;
+	},
+	isRGBString: function(color) {
+		color = color ? color : this._original;
 		let expression = /^rgb\((((((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]),\s?)){2}|((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5])\s)){2})((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]))|((((([1-9]?\d(\.\d+)?)|100|(\.\d+))%,\s?){2}|((([1-9]?\d(\.\d+)?)|100|(\.\d+))%\s){2})(([1-9]?\d(\.\d+)?)|100|(\.\d+))%))\)$/i;
-		return expression.test(str);
+		return expression.test(color);
 	},
 
-	isRGBA: function(str) {
+	isRGBAString: function(color) {
+		color = color ? color : this._original;
 		let expression = /^rgba\((((((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]),\s?)){3})|(((([1-9]?\d(\.\d+)?)|100|(\.\d+))%,\s?){3}))|(((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5])\s){3})|(((([1-9]?\d(\.\d+)?)|100|(\.\d+))%\s){3}))\/\s)((0?\.\d+)|[01]|(([1-9]?\d(\.\d+)?)|100|(\.\d+))%)\)$/i;
-		return expression.test(str);
+		return expression.test(color);
 	},
 
-	isHEX: function(str) {
+	isHEX: function(color) {
+		color = color ? color : this._original;
 		let expression = /^#([\da-f]{3}){1,2}$/i;
-		return expression.test(str);
+		return expression.test(color);
 	},
 
-	isHEXA: function(str) {
+	isHEXA: function(color) {
+		color = color ? color : this._original;
 		let expression = /^#([\da-f]{4}){1,2}$/i;
-		return expression.test(str);
+		return expression.test(color);
 	},
 
-	isHSL: function(str) {
+	isHSLString: function(color) {
+		color = color ? color : this._original;
 		let expression = /^hsl\(((((([12]?[1-9]?\d)|[12]0\d|(3[0-5]\d))(\.\d+)?)|(\.\d+))(deg)?|(0|0?\.\d+)turn|(([0-6](\.\d+)?)|(\.\d+))rad)((,\s?(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2}|(\s(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2})\)$/i;
-		return expression.test(str);
+		return expression.test(color);
 	},
 
-	isHSLA: function(str) {
+	isHSLAString: function(color) {
+		color = color ? color : this._original;
 		let expression = /^hsla\(((((([12]?[1-9]?\d)|[12]0\d|(3[0-5]\d))(\.\d+)?)|(\.\d+))(deg)?|(0|0?\.\d+)turn|(([0-6](\.\d+)?)|(\.\d+))rad)(((,\s?(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2},\s?)|((\s(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2}\s\/\s))((0?\.\d+)|[01]|(([1-9]?\d(\.\d+)?)|100|(\.\d+))%)\)$/i;
-		return expression.test(str);
+		return expression.test(color);
 	},
 
-	getFormat: function(str) {
-		if (this.isRGB(str) == true) return "RGB";
-		if (this.isRGBA(str) == true) return "RGBA";
-		if (this.isHEX(str) == true) return "HEX";
-		if (this.isHEXA(str) == true) return "HEXA";
-		if (this.isHSL(str) == true) return "HSL";
-		if (this.isHSLA(str) == true) return "HSLA";
-		console.error("Invalid input color: " + str);
+	getFormat: function(color) {
+		if (
+			color.hasOwnProperty("r") &&
+			color.hasOwnProperty("g") &&
+			color.hasOwnProperty("b")
+		) {
+			if (color.hasOwnProperty("a")) {
+				let color =
+					"rgba(" +
+					color.r +
+					"," +
+					color.g +
+					"," +
+					color.b +
+					"," +
+					a +
+					")";
+				if (this.isRGBAString(color) == true) return "RGBAObj";
+			}
+			let color = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
+			if (this.isRGBString(color) == true) return "RGBObj";
+		}
+
+		if (
+			color.hasOwnProperty("h") &&
+			color.hasOwnProperty("s") &&
+			color.hasOwnProperty("l")
+		) {
+			if (color.hasOwnProperty("a")) {
+				color =
+					"hsla(" +
+					color.h +
+					"," +
+					color.s +
+					"," +
+					color.l +
+					"," +
+					a +
+					")";
+				if (this.isHSLAString(color) == true) return "HSLAObj";
+			}
+			color = "hsl(" + color.h + "," + color.s + "," + color.l + ")";
+			if (this.isHSLString(color) == true) return "HSLObj";
+		}
+
+		if (typeof color === "string") {
+			if (this.isRGBString(color) == true) return "RGB";
+			if (this.isRGBAString(color) == true) return "RGBA";
+			if (this.isHEX(color) == true) return "HEX";
+			if (this.isHEXA(color) == true) return "HEXA";
+			if (this.isHSLString(color) == true) return "HSL";
+			if (this.isHSLAString(color) == true) return "HSLA";
+		}
+		console.error("Invalid input color: " + color);
 		return;
 	},
 
-	HEXToRGB: function(str, returnObj) {
-		if (this.isHEX(str)) {
+	HEXToRGB: function(color, returnObj) {
+		if (this.isHEX(color)) {
 			let r = 0,
 				g = 0,
 				b = 0;
 
-			if (str.length == 4) {
-				r = "0x" + str[1] + str[1];
-				g = "0x" + str[2] + str[2];
-				b = "0x" + str[3] + str[3];
-			} else if (str.length == 7) {
-				r = "0x" + str[1] + str[2];
-				g = "0x" + str[3] + str[4];
-				b = "0x" + str[5] + str[6];
+			if (color.length == 4) {
+				r = "0x" + color[1] + color[1];
+				g = "0x" + color[2] + color[2];
+				b = "0x" + color[3] + color[3];
+			} else if (color.length == 7) {
+				r = "0x" + color[1] + color[2];
+				g = "0x" + color[3] + color[4];
+				b = "0x" + color[5] + color[6];
 			}
 			if (returnObj) {
-				return {r: +r, g: +g, b: +b};
+				return { r: +r, g: +g, b: +b };
 			} else {
 				return "rgb(" + +r + "," + +g + "," + +b + ")";
 			}
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	HEXToHSL: function(str, returnObj) {
-		if (this.isHEX(str)) {
-			str = this.HEXToRGB(str);
-			return this.RGBToHSL(str);
+	HEXToHSL: function(color, returnObj) {
+		returnObj = returnObj === true;
+		if (this.isHEX(color)) {
+			color = this.HEXToRGB(color);
+			return this.RGBToHSL(color, returnObj);
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	HEXAToRGBA: function(str, returnObj) {
-		if (this.isHEXA(str)) {
+	HEXAToRGBA: function(color, returnObj) {
+		returnObj = returnObj === true;
+		if (this.isHEXA(color)) {
 			let r = 0,
 				g = 0,
 				b = 0;
 			a = 1;
 
-			if (str.length == 5) {
-				r = "0x" + str[1] + str[1];
-				g = "0x" + str[2] + str[2];
-				b = "0x" + str[3] + str[3];
-				a = "0x" + str[4] + str[4];
-			} else if (str.length == 9) {
-				r = "0x" + str[1] + str[2];
-				g = "0x" + str[3] + str[4];
-				b = "0x" + str[5] + str[6];
-				a = "0x" + str[7] + str[8];
+			if (color.length == 5) {
+				r = "0x" + color[1] + color[1];
+				g = "0x" + color[2] + color[2];
+				b = "0x" + color[3] + color[3];
+				a = "0x" + color[4] + color[4];
+			} else if (color.length == 9) {
+				r = "0x" + color[1] + color[2];
+				g = "0x" + color[3] + color[4];
+				b = "0x" + color[5] + color[6];
+				a = "0x" + color[7] + color[8];
 			}
 
 			a = +(a / 255).toFixed(3);
 
+			if (returnObj) {
+				return { r: +r, g: +g, b: +b, a: +a };
+			}
+
 			return "rgba(" + +r + "," + +g + "," + +b + "," + a + ")";
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	HEXAToHSLA: function(str, returnObj) {
-		if (this.isHEXA(str)) {
-			str = this.HEXAToRGBA(str);
-			return this.RGBAToHSLA(str);
-		} else {
-			console.error("Invalid input color: " + str);
-			return;
-		}
-	},
-
-	HSLToRGB: function(str, returnObj) {
+	HEXAToHSLA: function(color, returnObj) {
 		returnObj = returnObj === true;
-		if (this.isHSL(str)) {
-			let sep = str.indexOf(",") > -1 ? "," : " ";
-			str = str
+		if (this.isHEXA(color)) {
+			color = this.HEXAToRGBA(color);
+			return this.RGBAToHSLA(color, returnObj);
+		} else {
+			console.error("Invalid input color: " + color);
+			return;
+		}
+	},
+
+	HSLToRGB: function(color, returnObj) {
+		returnObj = returnObj === true;
+		if (this.isHSLString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
 				.substr(4)
 				.split(")")[0]
 				.split(sep);
 
-			let h = str[0],
-				s = str[1].substr(0, str[1].length - 1) / 100,
-				l = str[2].substr(0, str[2].length - 1) / 100;
+			let h = color[0],
+				s = color[1].substr(0, color[1].length - 1) / 100,
+				l = color[2].substr(0, color[2].length - 1) / 100;
 
 			if (h.indexOf("deg") > -1) {
 				h = h.substr(0, h.length - 3);
@@ -244,30 +401,36 @@ tincture.prototype = {
 			}
 			return "rgb(" + r + "," + g + "," + b + ")";
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	HSLToHEX: function(str) {
-		return null;
+	HSLToHEX: function(color) {
+		if (this.isHSLString(color)) {
+			color = this.HSLToRGB(color);
+			return this.RGBToHEX(color);
+		} else {
+			console.error("Invalid input color: " + color);
+			return;
+		}
 	},
 
-	HSLAToRGBA: function(str, returnObj) {
+	HSLAToRGBA: function(color, returnObj) {
 		returnObj = returnObj === true;
-		if (this.isHSLA(str)) {
-			let sep = str.indexOf(",") > -1 ? "," : " ";
-			str = str
+		if (this.isHSLAString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
 				.substr(5)
 				.split(")")[0]
 				.split(sep);
 
-			if (str.indexOf("/") > -1) str.splice(3, 1);
+			if (color.indexOf("/") > -1) color.splice(3, 1);
 
-			let h = str[0],
-				s = str[1].substr(0, str[1].length - 1),
-				l = str[2].substr(0, str[2].length - 1),
-				a = str[3];
+			let h = color[0],
+				s = color[1].substr(0, color[1].length - 1),
+				l = color[2].substr(0, color[2].length - 1),
+				a = color[3];
 
 			if (a.indexOf("%") > -1) {
 				a = +a.substr(0, a.length - 1) / 100;
@@ -291,46 +454,42 @@ tincture.prototype = {
 			if (returnObj) {
 				return obj;
 			}
-			return (
-				"rgba(" +
-				arr[0] +
-				"," +
-				arr[1] +
-				"," +
-				arr[2] +
-				"," +
-				a +
-				")"
-			);
+			return "rgba(" + obj.r + "," + obj.g + "," + obj.b + "," + a + ")";
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	HSLAToHEXA: function(str) {
-		return null;
+	HSLAToHEXA: function(color) {
+		if (this.isHSLAString(color)) {
+			color = this.HSLAToRGBA(color);
+			return this.RGBAToHEXA(color);
+		} else {
+			console.error("Invalid input color: " + color);
+			return;
+		}
 	},
 
-	RGBToHEX: function(str) {
-		if (this.isRGB(str)) {
-			let sep = str.indexOf(",") > -1 ? "," : " ";
-			str = str
+	RGBToHEX: function(color) {
+		if (this.isRGBString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
 				.substr(4)
 				.split(")")[0]
 				.split(sep);
 
-			for (let i in str) {
-				let r = str[i];
+			for (let i in color) {
+				let r = color[i];
 				if (r.indexOf("%") > -1)
-					str[i] = Math.round(
+					color[i] = Math.round(
 						(r.substr(0, r.length - 1) / 100) * 255
 					);
 			}
 
-			let r = (+str[0]).toString(16),
-				g = (+str[1]).toString(16),
-				b = (+str[2]).toString(16);
+			let r = (+color[0]).toString(16),
+				g = (+color[1]).toString(16),
+				b = (+color[2]).toString(16);
 
 			if (r.length == 1) r = "0" + r;
 			if (g.length == 1) g = "0" + g;
@@ -338,31 +497,31 @@ tincture.prototype = {
 
 			return "#" + r + g + b;
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	RGBToHSL: function(str, returnObj) {
+	RGBToHSL: function(color, returnObj) {
 		returnObj = returnObj === true;
-		if (this.isRGB(str)) {
-			let sep = str.indexOf(",") > -1 ? "," : " ";
-			str = str
+		if (this.isRGBString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
 				.substr(4)
 				.split(")")[0]
 				.split(sep);
 
-			for (let i in str) {
-				let r = str[i];
+			for (let i in color) {
+				let r = color[i];
 				if (r.indexOf("%") > -1)
-					str[i] = Math.round(
+					color[i] = Math.round(
 						(r.substr(0, r.length - 1) / 100) * 255
 					);
 			}
 
-			let r = str[0] / 255,
-				g = str[1] / 255,
-				b = str[2] / 255;
+			let r = color[0] / 255,
+				g = color[1] / 255,
+				b = color[2] / 255;
 
 			let cmin = Math.min(r, g, b),
 				cmax = Math.max(r, g, b),
@@ -397,78 +556,77 @@ tincture.prototype = {
 			}
 			return "hsl(" + h + "," + s + "%," + l + "%)";
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	RGBAToHEXA: function(str) {
-		if (this.isRGBA(str)) {
-			let sep = str.indexOf(",") > -1 ? "," : " ";
-			str = str
+	RGBAToHEXA: function(color) {
+		if (this.isRGBAString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
 				.substr(5)
 				.split(")")[0]
 				.split(sep);
 
-			if (str.indexOf("/") > -1) str.splice(3, 1);
+			if (color.indexOf("/") > -1) color.splice(3, 1);
 
-			for (let i in str) {
-				let r = str[i];
+			for (let i in color) {
+				let r = color[i];
 				if (r.indexOf("%") > -1) {
 					let p = r.substr(0, r.length - 1) / 100;
 
-					if (this.i < 3) {
-						str[i] = Math.round(p * 255);
+					if (i < 3) {
+						color[i] = Math.round(p * 255);
 					} else {
-						str[i] = p;
+						color[i] = p;
 					}
 				}
 			}
-			let r = (+str[0]).toString(16),
-				g = (+str[1]).toString(16),
-				b = (+str[2]).toString(16),
-				a = Math.round(+str[3] * 255).toString(16);
+			let r = (+color[0]).toString(16),
+				g = (+color[1]).toString(16),
+				b = (+color[2]).toString(16),
+				a = Math.round(+color[3] * 255).toString(16);
 
 			if (r.length == 1) r = "0" + r;
 			if (g.length == 1) g = "0" + g;
 			if (b.length == 1) b = "0" + b;
 			if (a.length == 1) a = "0" + a;
-
 			return "#" + r + g + b + a;
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	},
 
-	RGBAToHSLA: function(str, returnObj) {
+	RGBAToHSLA: function(color, returnObj) {
 		returnObj = returnObj === true;
-		if (this.isRGBA(str)) {
-			let sep = str.indexOf(",") > -1 ? "," : " ";
-			str = str
+		if (this.isRGBAString(color)) {
+			let sep = color.indexOf(",") > -1 ? "," : " ";
+			color = color
 				.substr(5)
 				.split(")")[0]
 				.split(sep);
 
-			if (str.indexOf("/") > -1) str.splice(3, 1);
+			if (color.indexOf("/") > -1) color.splice(3, 1);
 
-			for (let i in str) {
-				let r = str[i];
+			for (let i in color) {
+				let r = color[i];
 				if (r.indexOf("%") > -1) {
 					let p = r.substr(0, r.length - 1) / 100;
 
-					if (this.i < 3) {
-						str[i] = Math.round(p * 255);
+					if (i < 3) {
+						color[i] = Math.round(p * 255);
 					} else {
-						str[i] = p;
+						color[i] = p;
 					}
 				}
 			}
 
-			let r = str[0] / 255,
-				g = str[1] / 255,
-				b = str[2] / 255,
-				a = str[3];
+			let r = color[0] / 255,
+				g = color[1] / 255,
+				b = color[2] / 255,
+				a = color[3];
 
 			let cmin = Math.min(r, g, b),
 				cmax = Math.max(r, g, b),
@@ -500,11 +658,11 @@ tincture.prototype = {
 			l = +(l * 100).toFixed(1);
 
 			if (returnObj) {
-				return { h: +h, s: +s / 100, l: +l / 100, a: +a };
+				return { h: +h, s: +s, l: +l, a: +a };
 			}
 			return "hsl(" + h + "," + s + "%," + l + "%," + a + ")";
 		} else {
-			console.error("Invalid input color: " + str);
+			console.error("Invalid input color: " + color);
 			return;
 		}
 	}
